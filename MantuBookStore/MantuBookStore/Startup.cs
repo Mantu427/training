db2efb12ef94bf21a1594589e4718bd6d78e2f12
Mantu427 <mantusahu427@gmail.com>
2021-01-29 16:42:00 +0530

    my name is added

--- MantuBookStore/MantuBookStore/Startup.cs
+++ MantuBookStore/MantuBookStore/Startup.cs
@@ -32,7 +32,7 @@ namespace MantuBookStore
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World !");
+                    await context.Response.WriteAsync("Hello World Mantu Sahu!");
                 });
             });
         }
